91a60b7721db30cefbbbec3c938e6787f91b6ae2
Keith Smiley <keithbsmiley@gmail.com>
2022-05-10 11:45:00 -0700

    Remove unused file

--- jni_shim.c
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <stdio.h>
-
-extern void get_a_value_from_rust();
-
-JNIEXPORT jint JNICALL
-Java_com_example_androidapp_JniShim_printSomething(JNIEnv *env, jclass clazz) {
-    get_a_value_from_rust();
-}
