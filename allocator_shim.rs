29add17ade3c233132c692da76e8722e33dd70b8
Keith Smiley <keithbsmiley@gmail.com>
2026-03-17 22:56:05 -0700

    Modernize examples

---
+++ allocator_shim.rs
@@ -0,0 +1,2 @@
+// Empty crate — when compiled as a staticlib, rustc automatically includes
+// the allocator shim with correctly mangled symbols.
--- android_link_hack.c
+++ android_link_hack.c
@@ -1,6 +1,10 @@
 // https://github.com/bazelbuild/rules_rust/issues/1271
+#include <android/log.h>
+
 extern void android_link_hack(void);
 
 void call_link_hack(void) {
   android_link_hack();
+  // Just to verify this is built targeting android for debugging
+  __android_log_print(ANDROID_LOG_INFO, "RustDemo", "NDK link hack called");
 }
